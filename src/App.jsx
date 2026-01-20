0c676202f99280ddc9cfa227002ccf159bd23ba8
YasamSriCharan <2500030149@kluniversity.in>
2026-01-20 10:51:43 +0530

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "80px", fontFamily: "Arial" }}>
+        <h1>Herooooooo</h1>
+        <h4>Frontend Developer</h4>
+        <p><strong>contact no:</strong>9441631835 <br></br> <strong>Email:</strong>sricharanyasam157@gmail.com</p>
+      </div>
+    );
+  }
+}
+
+
+export default App;
