f50527646a4c023211ac410c0638574d2f6a9955
YasamSriCharan <2500030149@kluniversity.in>
2026-01-22 07:56:27 +0530

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ class App extends React.Component {
     return (
       <div style={{ textAlign: "center", marginTop: "80px", fontFamily: "Arial" }}>
         <h1>Herooooooo</h1>
-        <h2>side Herooooooo</h2>
+        <h2>side Heroooooo</h2>
         <h4>Frontend Developer</h4>
         <p><strong>contact no:</strong>9441631835 <br></br> <strong>Email:</strong>sricharanyasam157@gmail.com</p>
       </div>
